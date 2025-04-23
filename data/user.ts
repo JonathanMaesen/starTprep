ec18b7ca57ab9462763793910dd47c40e4ab8ba8
Nils-dev-mertens <nilsm2950@gmail.com>
2025-04-23 15:00:36 +0000

    fixed types

--- data/user.ts
+++ data/user.ts
@@ -1,9 +1,25 @@
 import { getAllDataMongoDB, insertOneObjMongodb, deleteElement, insertArrObjMongodb, getCollectionObj, updateElement, updateElements } from "./database";
-import mongodb, { ObjectId } from "mongodb";
-import {Ingredient, Dish, Floorelement, User} from "./types";
+import {Token, User} from "./types";
 let users : User[];
+let sessions: Token[];
 async function getUsers() {
     users = await getAllDataMongoDB("users");
-    console.log(users);
 }
 getUsers();
+
+export function checkUserPassword(idin:number, password:string):boolean {
+    const userindex:number = users.findIndex((e) => { return e.id == idin});
+    if(userindex == -1){
+        console.log("index is wrong" + userindex);
+        return false;
+    }
+    if(users[userindex].password != password){
+        console.log("password is wrong");
+        return false;
+    }
+    return true;
+}
+
+export function checkSessions(token:any) {
+    
+}
