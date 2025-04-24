6515229dc04ff96aae3bc1095477829c0a8fbbca
JonathanMaesen <tiwaztyr1802@gmail.com>
2025-04-24 18:42:01 +0000

    had to switch branch to FOH cause the database update fucked me

--- routes/foh.ts
+++ routes/foh.ts
@@ -1,10 +1,49 @@
 import express, { Router } from "express";
-
+import { floor, pushFloorElement } from "../data/datausages";
+import { Floorelement } from "../data/types";
+import { table } from "console";
 const router: Router = express.Router();
 
-router.get("/", (req, res) => {
+
+router.get("/", async (req, res) => {
     res.render("foh", {
+        tables: floor
     });
+});
+router.post("/sortfloor", async (req, res) => {
+    const sortingdirection = req.body.sortOptions;
+    if (sortingdirection === "asc") {
+        floor.sort((a, b) => parseInt(a.follownummer) - parseInt(b.follownummer));
+    } else if (sortingdirection === "desc") {
+        floor.sort((a, b) => parseInt(b.follownummer) - parseInt(a.follownummer));
+    }else if (sortingdirection === "chairsAsc"){
+        floor.sort((a, b) => parseInt(a.amount.toString()) - parseInt(b.amount.toString()));
+    }else if (sortingdirection === "chairsDesc"){
+        floor.sort((a, b) => parseInt(b.amount.toString()) - parseInt(a.amount.toString()));
+    }
+    res.redirect("/foh");
+});
+router.post("/addtable", async (req, res) => {
+    try {
+        const tableNumbers: string[] = floor.map(table => table.follownummer);
+        let outOfOrderElement: string | null = null;
+        for (let i = 0; i < tableNumbers.length - 1; i++) {
+            if (parseInt(tableNumbers[i]) > parseInt(tableNumbers[i + 1])) {
+                outOfOrderElement = tableNumbers[i + 1];
+                break;
+            }
+        }
+        const newtable : Floorelement = {
+            follownummer: outOfOrderElement || (parseInt(tableNumbers[tableNumbers.length - 1]) + 1).toString(),
+            amount: 0
+        }
+        await console.log(newtable)
+        res.redirect("/foh");
+
+    } catch (error) {
+        console.error("Error adding table:", error);
+        res.status(500).send("internal server errro");
+    }
 });
 
 export default router;
